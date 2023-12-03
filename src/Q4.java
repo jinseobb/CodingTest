419eb23fb806242e87b624e5cf220e74cca5cba6
연진섭 <ikc9063@gmail.com>
2023-12-03 11:32:16 +0900

    숫자 입력받아 해당 숫자만큼 단어 입력하여 입력된 단어들 뒤집는 문제(내가 푼 버전)

--- src/Q4.java
+++ src/Q4.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-
 import java.util.Scanner;
 
 public class Q4 {
@@ -10,30 +9,44 @@ public class Q4 {
 		
 		
 		Q4 t = new Q4();
-		
+	
+		String i1 = "";
+		String[] a1 = {}; 
+			
 		
 		Scanner scan = new Scanner(System.in);
+		int i2 = scan.nextInt();
 		
-		String i1 = scan.nextLine();
-		
-		
-		
-		System.out.println(t.solution(i1));
+	   if(i2 <= 20 && i2 >= 3) {
+		   a1 = new String[i2];
+		   for(int i=0; i<i2; i++) {
+			   Scanner scan2 = new Scanner(System.in);
+				i1 = scan2.nextLine();
+			    a1[i] = i1;
+			    
+		   } 
+	   }
+	
+	  //System.out.println(Arrays.toString(a1));
+	   for(String x : t.solution(i2,a1)) {
+		   System.out.println(x);
+	   }
+
 		
 	}
 
+	private ArrayList<String> solution(int i2, String[] a1) {
 
+ 		ArrayList<String> answer = new ArrayList<String>();
+ 		
+ 		for(String x : a1) {
+ 			String tmp = new StringBuilder(x).reverse().toString();
+ 		answer.add(tmp);
+ 		}
+ 		
+		return answer;
+	}
 
-	
-
-
-	
-
-	
-
-	
-
-	
 }
 	
 	
