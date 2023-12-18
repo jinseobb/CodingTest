5b83cc3df47bbe88934f61609d113a2825554d8f
연진섭 <ikc9063@gmail.com>
2023-12-18 22:38:49 +0900

    백준 11654 아스키코드변환문제

---
+++ src/BaekJoon/String/Q2.java
@@ -0,0 +1,29 @@
+package BaekJoon.String;
+
+import java.util.Scanner;
+
+public class Q2 {
+
+	public static void main(String[] args) {
+		
+		Q2 t = new Q2();
+		
+		Scanner scan= new Scanner(System.in);
+		
+		char i = scan.next().charAt(0);
+	
+		System.out.println(t.solution(i));
+	}
+
+	private int solution(char i) {
+
+		int answer = 0;
+		
+	answer = (int)i;
+		
+		return answer;
+	}
+	
+	
+	
+}
