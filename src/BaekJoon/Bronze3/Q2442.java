ef050e29d9b45113d52469979d0802d61d20cacf
연진섭 <ikc9063@gmail.com>
2023-12-19 00:09:42 +0900

    백준 2442 별찍기

---
+++ src/BaekJoon/Bronze3/Q2442.java
@@ -0,0 +1,28 @@
+package BaekJoon.Bronze3;
+
+import java.util.Scanner;
+
+public class Q2442 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+		
+			for(int j=n; j>i; j--) {
+				System.out.print(" ");
+			}
+			
+			for(int j=1; j<=2*i-1; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println(" ");
+		}
+		
+		
+	}
+}
