565f2d6aa05fafaf3fa1b50480a442c31fcb5165
연진섭 <ikc9063@gmail.com>
2024-01-24 21:25:33 +0900

    백준 10816 숫자카드2 문제 복습

--- src/BaekJoon/Practice.java
+++ src/BaekJoon/Practice.java
@@ -3,47 +3,65 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Practice {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-	int num = 1;
-	Boolean b = true;
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	int m = Integer.parseInt(br.readLine());
-	StringBuilder sb = new StringBuilder();
-	int[] arr = new int[m];
-	for(int i=0; i < m;i ++) {
-	 arr[i] = Integer.parseInt(br.readLine());
-	}
-	
-	Stack<Integer> s = new Stack<>();
-	for(int i=0; i<m; i++) {
-		int k = arr[i];
-		if(k>= num) {
-		while(k>=num) {
-			s.push(num++);
-			sb.append("+").append("\n");
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n        = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		s.pop();
-		sb.append("-").append("\n");
+		Arrays.sort(arr);
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<m; i++) {
+			int target = Integer.parseInt(st.nextToken());
+			sb.append(Upper(arr,target) - Lower(arr,target)).append(" ");
+		}
+	
+	  System.out.println(sb);
+	}
+
+	private static int Lower(int[] arr, int target) {
+
+		int start = 0;
+		int end = arr.length;
+		
+		while(start < end) {
+		int mid = start + (end - start)/2;	
+		
+		if(arr[mid]<= target ) {
+			start = mid + 1;
 		}else {
-			int kk = s.pop();
-			if(kk > num) {
-				System.out.println("NO");
-				b = false;
-				break;
-			}else {
-				sb.append("-").append("\n");
-			}
+			end = mid;
 		}
-	}
-	if(b) {
-		System.out.println(sb);	
-	}
 	
+		}
+		
+		return end;
+	}
+
+	private static int Upper(int[] arr, int target) {
+		int start = 0;
+		int end = arr.length;
+
+		while(start < end) {
+		int mid = start + (end - start)/2;	
+		if(arr[mid] < target) {
+			start = mid + 1;
+		}else {
+			end = mid;
+		}
+		}
+		
+		return end;
 	}
 	
 }
