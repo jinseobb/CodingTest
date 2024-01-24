f39a678e74d960ab4ae062f8c48a6568e8abe2b3
연진섭 <ikc9063@gmail.com>
2024-01-24 21:11:08 +0900

    백준 1874 스택수열 복습

--- src/BaekJoon/Practice.java
+++ src/BaekJoon/Practice.java
@@ -9,38 +9,41 @@ import java.util.StringTokenizer;
 public class Practice {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
+	int num = 1;
+	Boolean b = true;
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int m = Integer.parseInt(br.readLine());
+	StringBuilder sb = new StringBuilder();
+	int[] arr = new int[m];
+	for(int i=0; i < m;i ++) {
+	 arr[i] = Integer.parseInt(br.readLine());
+	}
 	
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int m = Integer.parseInt(br.readLine());
-		for(int i=0; i<m; i++) {
-			Stack<String> s = new Stack<>();
-			Boolean b = true;
-		//	StringTokenizer st = new StringTokenizer(br.readLine());
-			String tmp = br.readLine();
-			
-			for(int j=0; j<tmp.length(); j++) {
-							
-				char c = tmp.charAt(j);
-				if( c == '(') {
-					s.push(String.valueOf(c));
-				}else  if(c == ')'){
-					if(!s.isEmpty() && s.peek().equals("(")) {
-					s.pop();	
-					}else {
-						b = false;
-					}
-				}
-			}
-			if(s.isEmpty()&& b) {
-				sb.append("YES").append("\n");
+	Stack<Integer> s = new Stack<>();
+	for(int i=0; i<m; i++) {
+		int k = arr[i];
+		if(k>= num) {
+		while(k>=num) {
+			s.push(num++);
+			sb.append("+").append("\n");
+		}
+		s.pop();
+		sb.append("-").append("\n");
+		}else {
+			int kk = s.pop();
+			if(kk > num) {
+				System.out.println("NO");
+				b = false;
+				break;
 			}else {
-				sb.append("NO").append("\n");
+				sb.append("-").append("\n");
 			}
 		}
-		
-		System.out.println(sb);
+	}
+	if(b) {
+		System.out.println(sb);	
+	}
+	
 	}
 	
 }
