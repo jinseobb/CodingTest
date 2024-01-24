3fffd24d160325073b1dd1cd1cdc68452ba052f5
연진섭 <ikc9063@gmail.com>
2024-01-24 21:35:29 +0900

    백준 10816 숫자 카드2 문제 해쉬맵으로 복습

--- src/BaekJoon/Practice.java
+++ src/BaekJoon/Practice.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -11,57 +12,25 @@ public class Practice {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		StringBuilder sb = new StringBuilder();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n        = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
+	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	 int n = Integer.parseInt(br.readLine());
+	 StringTokenizer st = new StringTokenizer(br.readLine());
+	 HashMap<Integer,Integer> map = new HashMap<>();
 		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		int m = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<m; i++) {
-			int target = Integer.parseInt(st.nextToken());
-			sb.append(Upper(arr,target) - Lower(arr,target)).append(" ");
+			int key = Integer.parseInt(st.nextToken());
+			map.put(key, map.getOrDefault(key, 0) + 1);
 		}
 	
-	  System.out.println(sb);
-	}
-
-	private static int Lower(int[] arr, int target) {
-
-		int start = 0;
-		int end = arr.length;
 		
-		while(start < end) {
-		int mid = start + (end - start)/2;	
 		
-		if(arr[mid]<= target ) {
-			start = mid + 1;
-		}else {
-			end = mid;
-		}
-	
-		}
-		
-		return end;
-	}
-
-	private static int Upper(int[] arr, int target) {
-		int start = 0;
-		int end = arr.length;
-
-		while(start < end) {
-		int mid = start + (end - start)/2;	
-		if(arr[mid] < target) {
-			start = mid + 1;
-		}else {
-			end = mid;
-		}
-		}
-		
-		return end;
+    int m = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    for(int i=0; i<m; i++) {
+    	int k = Integer.parseInt(st.nextToken());
+    	sb.append(map.getOrDefault(k, 0)).append(" ");
+    }
+   System.out.println(sb);
+   
 	}
 	
 }
