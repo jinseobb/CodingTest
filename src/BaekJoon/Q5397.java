81f404d0a2988ab8219d651dc3524fe331c98107
연진섭 <ikc9063@gmail.com>
2024-02-04 12:06:26 +0900

    백준 5397번 키로거 문제

--- src/BaekJoon/Q5397.java
+++ src/BaekJoon/Q5397.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Q5397 {
-	
+	//키로거 
 	static String key;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
