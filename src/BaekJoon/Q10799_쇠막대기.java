9d3598b05a8221eec2f538fa62e406058da4409b
연진섭 <ikc9063@gmail.com>
2024-02-05 23:26:25 +0900

    백준 쇠막대기 문제 (스택활용)

---
+++ src/BaekJoon/Q10799_쇠막대기.java
@@ -0,0 +1,30 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Q10799_쇠막대기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String tmp = br.readLine();
+		Stack<Character> stack = new Stack<>();
+		long count = 0;
+		for(int i=0 ;i<tmp.length(); i++) {
+			char c = tmp.charAt(i);
+			if(c == '(') {
+				stack.push(c);
+			}
+			if(c ==')') {
+				stack.pop();
+				if(tmp.charAt(i-1) == '(') {
+					count += stack.size();
+				}else if(tmp.charAt(i-1) == ')'){
+					count += 1;
+				}
+			}
+		}
+		System.out.println(count);
+	} //main
+}
