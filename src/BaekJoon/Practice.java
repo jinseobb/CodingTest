2dd17df3b7e64f65ab77749f6b3f11f967ce7e45
연진섭 <ikc9063@gmail.com>
2024-02-07 21:56:47 +0900

    백준 6198 옥상 정원 꾸미기 문제 복습

--- src/BaekJoon/Practice.java
+++ src/BaekJoon/Practice.java
@@ -3,47 +3,33 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Practice {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-	StringBuilder sb = new StringBuilder();
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	StringTokenizer st = new StringTokenizer(br.readLine());
 	
-    int count = 0;
-    int n = Integer.parseInt(st.nextToken());
-    int m = Integer.parseInt(st.nextToken());
-    HashMap<String,Integer>map1 = new HashMap<>();
-    HashMap<String,Integer>map2 = new HashMap<>();
-    
-    for(int i=0; i<n; i++) {
-    	String tmp = br.readLine();
-    	map1.put(tmp, map1.getOrDefault(tmp, 0) + 1);
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer[]> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        Integer n = Integer.parseInt(br.readLine());
+        int count = 0;
+        for(Integer i=0; i<n; i++) {
+        	Integer num = Integer.parseInt(br.readLine());
+        	while(!stack.isEmpty()) {
+        		if(stack.peek()[1] <= num) {
+        			stack.pop();
+        		}else {
+        			break;
+        		}
+        	}
+        	count += stack.size();
+        
+        	stack.push(new Integer[] {i,num});
+        }
+        
+        System.out.println(sb);
     }
-	
-    for(int i=0; i<m; i++) {
-    	String tmp = br.readLine();
-    	map2.put(tmp, map2.getOrDefault(tmp, 0) +  1);
-    	if(map1.containsKey(tmp)) {
-    		count++;
-    	}
-    		
-    	
-    }
     
-    sb.append(count).append("\n");
-    
-    for(String ket : map1.keySet()) {
-    	if(map2.containsKey(ket)) {
-    		sb.append(ket).append("\n");
-    	}
-    }
-    System.out.println(sb);
-	}
 	
 }
