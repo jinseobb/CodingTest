3aefc87ec2ffc17e12b89ac11f3daa2e91803847
연진섭 <ikc9063@gmail.com>
2024-02-07 21:34:50 +0900

    백준 10799 쇠막대기 문제 복습

--- src/BaekJoon/Q10799_쇠막대기.java
+++ src/BaekJoon/Q10799_쇠막대기.java
@@ -11,20 +11,25 @@ public class Q10799_쇠막대기 {
 		String tmp = br.readLine();
 		Stack<Character> stack = new Stack<>();
 		long count = 0;
-		for(int i=0 ;i<tmp.length(); i++) {
+		for(int i=0; i<tmp.length(); i++) {
 			char c = tmp.charAt(i);
+			
 			if(c == '(') {
 				stack.push(c);
 			}
-			if(c ==')') {
+				
+			
+			if( c == ')') {
 				stack.pop();
 				if(tmp.charAt(i-1) == '(') {
+					
 					count += stack.size();
-				}else if(tmp.charAt(i-1) == ')'){
+				}else if (tmp.charAt(i-1)== ')') {
+				
 					count += 1;
 				}
 			}
-		}
+		} // for
 		System.out.println(count);
 	} //main
 }
