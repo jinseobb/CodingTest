2a401ade0f422cbe0acef8a9afd159d1d57f20a9
연진섭 <ikc9063@gmail.com>
2024-02-19 21:19:31 +0900

    백준 2606 바이러스 복습

--- src/BaekJoon/Practice.java
+++ src/BaekJoon/Practice.java
@@ -3,99 +3,65 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Practice{
-	static int dx[] = {1,0,-1,0};
-	static int dy[] = {0,1,0,-1};
-	static boolean[][] visited;
-	static int[][]arr;
-	static int n,m ,count, area,max;
-	static Queue<Point> q;
-	static class Point {
-		private int x;
-		private int y;
-		public Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-		public int getX() {
-			return x;
-		}
-		
-		public int getY() {
-			return y;
-		}
-		
-		public void setX(int x){
-	    	this.x = x;
-	    }
-	    
-	    public void setY(int y){
-	    	this.y = y;
-	    }
-
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		arr = new int[n][m];
-		visited = new boolean[n][m];
+	 static int n,m,count;
+	 static int[] arr;
+	 static boolean[] visited;
+	 static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	 static Queue<Pair> q ;
+	 static class Pair{
+		 private int x;
+		 public Pair(int x) {
+			 this.x = x;
+		 }
+	 }
+	 
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br = new  BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		arr = new int[n+1];
+		visited = new boolean[n+1];
 		q = new LinkedList<>();
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<m; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
+		for(int i=0; i<=n; i++) {
+			graph.add(new ArrayList<Integer>());
 		}
 		
-		//start
+		for(int i=0; i<m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph.get(a).add(b);
+			graph.get(b).add(a);
+			
+		}
 		count = 0;
-		area = 0;
-		max = 0;
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<m; j++) {
-				if(arr[i][j] == 0 || visited[i][j]) {
-					continue;
-				}
-				
-				count++;
-				q.offer(new Point(i,j));
-				visited[i][j] = true;
-				area = 0;
-				
-				
-					while(!q.isEmpty()) {
-						Point p = q.poll();
-						area++;
-						for(int k=0; k<4; k++) {
-							int nx = p.x + dx[k];
-							int ny = p.y + dy[k];
-							if(nx < 0 || ny < 0 || nx >= n || ny >= m) {
-								continue;
-							}
-							
-							if(arr[nx][ny] == 1 && !visited[nx][ny]) {
-								q.offer(new Point(nx,ny));
-								visited[nx][ny] = true;
-							}
-						}
+		q.offer(new Pair(1));
+		visited[1] = true;
+		
+		while(!q.isEmpty()) {
+			Pair p = q.poll();
+			//count++;
+			for(int i=0; i<graph.get(p.x).size();i++) {
+				int next = graph.get(p.x).get(i);
+				if(!visited[next]) {
 					
-					if(area > max) {
-						max = area;
-					}
+					q.offer(new Pair(next));
+					visited[next] = true;
+					count++;
 				}
 			}
 		}
 		System.out.println(count);
-		System.out.println(max);
+	  }
+
 		
-		
-	}
 
 }
+
+
