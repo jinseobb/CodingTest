b79541eca8574a9a4337c2107a9202b9877229dd
연진섭 <ikc9063@gmail.com>
2024-02-22 22:55:21 +0900

    백준 2644 촌수계산 복습 - BFS

--- src/BaekJoon/Practice.java
+++ src/BaekJoon/Practice.java
@@ -9,11 +9,12 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Practice{
-	 static int n,m,count;
-	 static int[] arr;
+	 static int n,k,h,m,count;
+	 static int[]arr;
+	 static int[] index;
 	 static boolean[] visited;
 	 static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-	 static Queue<Pair> q ;
+	 static Queue<Integer> q  = new LinkedList<>();
 	 static class Pair{
 		 private int x;
 		 public Pair(int x) {
@@ -22,46 +23,53 @@ public class Practice{
 	 }
 	 
 	public static void main(String[] args) throws NumberFormatException, IOException{
-		BufferedReader br = new  BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		k = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(br.readLine());
 		arr = new int[n+1];
-		visited = new boolean[n+1];
-		q = new LinkedList<>();
 		for(int i=0; i<=n; i++) {
 			graph.add(new ArrayList<Integer>());
 		}
 		
 		for(int i=0; i<m; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			graph.get(a).add(b);
 			graph.get(b).add(a);
-			
 		}
-		count = 0;
-		q.offer(new Pair(1));
-		visited[1] = true;
 		
+		count = 0;
+		visited = new boolean[n+1];
+		int result = bfs(k);
+		System.out.println(result);
+		
+	}
+
+	private static int bfs(int start) {
+		q.offer(start);
+		visited[start] = true;
 		while(!q.isEmpty()) {
-			Pair p = q.poll();
-			//count++;
-			for(int i=0; i<graph.get(p.x).size();i++) {
-				int next = graph.get(p.x).get(i);
+			int first = q.poll();
+			if(first == h) {
+				return arr[first];
+			}
+			for(int i=0; i<graph.get(first).size();i++) {
+				int next = graph.get(first).get(i);
 				if(!visited[next]) {
-					
-					q.offer(new Pair(next));
+					q.offer(next);
 					visited[next] = true;
-					count++;
+					arr[next] = arr[first]+1;
 				}
 			}
 		}
-		System.out.println(count);
-	  }
+		return -1;
+	}
 
-		
-
+	
 }
 
 
