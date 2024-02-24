298f8eeb1e745415ba938d69ebc38aecbeb34b19
연진섭 <ikc9063@gmail.com>
2024-02-24 17:44:22 +0900

    백준 7569 토마토3차원 복습 - BFS

--- src/BaekJoon/Practice.java
+++ src/BaekJoon/Practice.java
@@ -3,73 +3,79 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Practice{
-	 static int n,k,h,m,count;
-	 static int[]arr;
-	 static int[] index;
-	 static boolean[] visited;
-	 static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-	 static Queue<Integer> q  = new LinkedList<>();
-	 static class Pair{
-		 private int x;
-		 public Pair(int x) {
-			 this.x = x;
-		 }
-	 }
-	 
-	public static void main(String[] args) throws NumberFormatException, IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		k = Integer.parseInt(st.nextToken());
-		h = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(br.readLine());
-		arr = new int[n+1];
-		for(int i=0; i<=n; i++) {
-			graph.add(new ArrayList<Integer>());
+public class Practice {
+	static int[] dx = {1,0,-1,0,0,0};
+	static int[] dy = {0,1,0,-1,0,0};
+	static int[] dz = {0,0,0,0,1,-1};
+	static int n,m,h,count,finalcount= 0;
+	static Queue<Pair> q = new LinkedList<>();
+	static int[][][] arr;
+	static boolean[][][] visited;
+	static class Pair{
+		private int x;
+		private int y;
+		private int z;
+		private int day;
+		public Pair(int x, int y, int z , int day) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.day = day;
 		}
-		
-		for(int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			graph.get(a).add(b);
-			graph.get(b).add(a);
-		}
-		
-		count = 0;
-		visited = new boolean[n+1];
-		int result = bfs(k);
-		System.out.println(result);
-		
 	}
-
-	private static int bfs(int start) {
-		q.offer(start);
-		visited[start] = true;
-		while(!q.isEmpty()) {
-			int first = q.poll();
-			if(first == h) {
-				return arr[first];
-			}
-			for(int i=0; i<graph.get(first).size();i++) {
-				int next = graph.get(first).get(i);
-				if(!visited[next]) {
-					q.offer(next);
-					visited[next] = true;
-					arr[next] = arr[first]+1;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		m = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+		arr= new int[h][n][m];
+		visited = new boolean [h][n][m];
+		for(int i=0; i<h; i++) {
+			for(int j=0;j<n;j++) {
+				st = new StringTokenizer(br.readLine());
+				for(int k=0; k<m; k++) {
+					arr[i][j][k] = Integer.parseInt(st.nextToken());
+					if(arr[i][j][k]==0) {
+						count++;
+					}
+					if(arr[i][j][k] == 1) {
+						q.offer(new Pair(j,k,i,0));
+						visited[i][j][k] = true;
+					}
 				}
 			}
 		}
-		return -1;
+		
+		//bfs
+		while(!q.isEmpty()) {
+			Pair p = q.poll();
+			for(int i=0; i<6; i++) {
+				int nx = p.x+dx[i];
+				int ny = p.y+dy[i];
+				int nz = p.z+dz[i];
+				if(nx < 0 || ny < 0 || nz < 0 || nx >=n || ny >=m || nz>=h){
+					continue;
+				}
+				if(!visited[nz][nx][ny] && arr[nz][nx][ny] == 0) {
+					q.offer(new Pair(nx,ny,nz,p.day+1));
+					visited[nz][nx][ny] = true;
+					finalcount = p.day+1;
+					count--;
+							
+				}
+			}
+		}
+		
+		if(count ==0) {
+			System.out.println(count);
+		}else {
+			System.out.println(-1);
+		}
+		
 	}
-
-	
-}
-
-
+}	
