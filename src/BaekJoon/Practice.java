0d6326dc3e37923d18b9386d0d444f559370a3a0
연진섭 <ikc9063@gmail.com>
2024-05-09 10:44:41 +0900

    Q14503 로봇청소기 복습풀이

--- src/BaekJoon/Practice.java
+++ src/BaekJoon/Practice.java
@@ -1,93 +1,119 @@
 package BaekJoon;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-import BaekJoon.Q15683_감시.Pair;
-
 public class Practice {
-	static int n,m,count,r,c,result;
-	static int[][] notebook, sticker;
-	
-	static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        count = Integer.parseInt(st.nextToken());
-        notebook = new int[n][m];
-        for(int i=0; i<count;i++) {
-        	st = new StringTokenizer(br.readLine());
-        	 r = Integer.parseInt(st.nextToken());
-        	c = Integer.parseInt(st.nextToken());
-        	sticker = new int[r][c];
-        	for(int j=0; j<r; j++) {
-        		st = new StringTokenizer(br.readLine());
-        		for(int k=0; k<c; k++) {
-        			sticker[j][k] = Integer.parseInt(st.nextToken());
-        		}
-        	}
-        	//4방향 
-        	for(int rotate = 0; rotate<4; rotate++) {
-        		
-        		if(attach(sticker)) {
-        			break;
-        		}
-        		sticker = rotate(sticker);
-        	}
-        
-        }
-        System.out.println(result);
-    }
-	private static int[][] rotate(int[][] sticker2) {
-		int r = sticker2.length;
-		int c = sticker2[0].length;
-		int[][] newsticker = new int[c][r];
-		for(int i=0; i<r; i++) {
-			for(int j=0; j<c;j++) {
-				newsticker[j][r-1-i]=sticker2[i][j];
-			}
-		}
-		return newsticker;
-	}
-	private static boolean attach(int[][] sticker2) {
-		for(int i=0; i<n-sticker2.length+1;i++) {
-			for(int j=0; j<m-sticker2[0].length+1;j++) {
-				if(check(sticker2,i,j)) {
-					attachOne(sticker2,i,j);
-					return true;
-				}
+	static int N,M,r,c,d,count,nextDirection;
+	static int[][] arr;
+	static boolean[][] cleaned;	
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,1,0,-1};
+	static Queue<Pair> q;
+ 	static class Pair{
+ 		private int x;
+ 		private int y;
+ 		private int direction;
+ 		
+ 		public Pair(int x, int y, int direction) {
+ 			this.x = x;
+ 			this.y = y;
+ 			this.direction = direction;
+ 		}
+ 		
+ 	}
+ 	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		cleaned = new boolean[N][M];
+		q = new LinkedList<Pair>();
+		
+		st = new StringTokenizer(br.readLine());
+		r = Integer.parseInt(st.nextToken());
+		c =Integer.parseInt(st.nextToken());
+		d =Integer.parseInt(st.nextToken());
+		
+		//d = 북동남서 
+		//0 이면 청소x 1 이면 벽  
+		
+		//map 채우
+		for(int i=0; i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<M;j++) {
+				int ch = Integer.parseInt(st.nextToken());
+				arr[i][j] = ch;
 			}
 		}
 		
-		return false;
+		
+		q.offer(new Pair(r,c,d));
+		cleaned[r][c] = true;
+		count = 1;
+		BFS();
+		
 	}
-	private static void attachOne(int[][] sticker2, int row, int col) {
 
-		for(int i=0; i<sticker2.length;i++) {
-			for(int j=0; j<sticker2[0].length;j++) {
-				if(sticker2[i][j] == 1) {
-					notebook[row+i][col+j] = sticker2[i][j];
-					result++;
+	private static void BFS() {
+		while(!q.isEmpty()) {
+			Pair p = q.poll();
+			nextDirection = p.direction;
+			boolean moved = false;
+			//1. 청소되지 않은 경우 청소 하기 
+			if(arr[p.x][p.y] == 0) {
+				arr[p.x][p.y] = 2;
+			}
+			
+			//2. 왼쪽 방향으로 회전 
+			for(int i=0; i<4; i++) {
+				nextDirection  = (nextDirection+3) % 4;
+				int nx = p.x+dx[nextDirection];
+				int ny = p.y+dy[nextDirection];
+				if(nx < 0 || ny < 0 || nx >= N|| ny >= M) {
+					continue;
 				}
+				if(arr[nx][ny] == 0 && !cleaned[nx][ny]) {
+					cleaned[nx][ny] = true;
+					q.offer(new Pair(nx,ny,nextDirection));
+					count++;
+					moved = true;
+					break;
+				}
+				
+			}
+			if(!moved) {
+				nextDirection = (nextDirection+2)%4;
 			}
 		}
+		
+		
 	}
-	private static boolean check(int[][] sticker2, int row, int col) {
-		for(int i=0; i<sticker2.length;i++) {
-			for(int j=0; j<sticker2[0].length;j++) {
-				if(sticker2[i][j] == 1 && notebook[row+i][col+j] == 1) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-    
 	
-
-}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
